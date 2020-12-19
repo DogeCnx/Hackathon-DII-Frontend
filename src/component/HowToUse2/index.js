3f67576cc0393990a7477fa9ceeb706004bf8b49
Ramita <ramitakahapana@gmail.com>
2020-12-19 15:24:03 +0700

    EDIT howtouse2

--- src/component/HowToUse2/index.js
+++ src/component/HowToUse2/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+// const ContainerToUse =styled.div``
+const Content = styled.ol``;
+const TextNumber = styled.li``;
 
-function HowToUse2 (){
-    return (
-        <>
-        </>
-    )
+function HowToUse2() {
+  return (
+    <Content>
+      <TextNumber>เลือกอาการ</TextNumber>
+      <TextNumber>เลือกยาน้ำหรือยาเม็ด</TextNumber>
+      <TextNumber>จ่ายเงิน</TextNumber>
+      <TextNumber>รอรับยา</TextNumber>
+    </Content>
+  );
 }
 
-export default HowToUse2
+export default HowToUse2;
